fc22e1ee7eadd4819874e1c513d58f85e959f8b3
RobertoCH44 <71985665+RobertoCH44@users.noreply.github.com>
2024-09-28 23:08:49 -0600

    Scripts y Escena de "DrawTest" Removido
    - Scripts y Escena de "DrawTest" Removido.
    - Cambio en colisiones de primera escena "Intro1"
    - Caja musical actualizada, se activa cuando el jugador entra al colliderTrigger

--- Assets/CajaMusical.cs
+++ Assets/CajaMusical.cs
@@ -14,7 +14,7 @@ public class CajaMusical : MonoBehaviour
     Vector3 gInitialPosition;
     PlayerController playerController;
 
-    public bool active;
+    public bool active,playerDetected;
     public float timer;
     public float timeToStop,timeToRestart;
 
@@ -27,51 +27,80 @@ public class CajaMusical : MonoBehaviour
 
     void Update()
     {
-        if (active)
+        if (playerDetected)
         {
-            timer += Time.deltaTime;
-            //Ghost Regresa al lugar de origen
-            Vector2 direction = (gInitialPosition - ghost.position).normalized;
-            ghost.position = Vector2.MoveTowards(ghost.position, gInitialPosition, 15 * Time.deltaTime);
-
-            if (timer >= timeToStop)
+            if (active)
             {
-                timer = 0;
-                active = false;
-                StopMusicalBox();
+                timer += Time.deltaTime;
+                //Ghost Regresa al lugar de origen
+                Vector2 direction = (gInitialPosition - ghost.position).normalized;
+                ghost.position = Vector2.MoveTowards(ghost.position, gInitialPosition, 15 * Time.deltaTime);
+
+                if (timer >= timeToStop)
+                {
+                    timer = 0;
+                    active = false;
+                    StopMusicalBox();
+                }
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+
+                //Accion de ghost cuando el jugador se mueve
+                if (playerController.horizontal != 0)
+                {
+                    Vector2 direction = (player.transform.position - ghost.position).normalized;
+                    ghost.position = Vector2.MoveTowards(ghost.position, player.transform.position, speed * Time.deltaTime);
+                    if (ghost.position.x > player.transform.position.x)
+                    {
+                        ghostSprite.flipX = true;
+                    }
+                    else
+                    {
+                        ghostSprite.flipX = false;
+                    }
+                    ghostAnim.SetBool("Attacking", true);
+                }
+                else
+                {
+                    ghostAnim.SetBool("Attacking", false);
+                }
+
+                if (timer <= timeToRestart)
+                {
+                    timer = 0;
+                    active = true;
+                    PlayMusicalBox();
+                }
             }
         }
         else
         {
-            timer -= Time.deltaTime;
+            ghost.position = Vector2.MoveTowards(ghost.position, gInitialPosition, 8 * Time.deltaTime);
+        }           
+    }
 
-            //Accion de ghost cuando el jugador se mueve
-            if (playerController.horizontal != 0)
-            {               
-                Vector2 direction = (player.transform.position - ghost.position).normalized;
-                ghost.position = Vector2.MoveTowards(ghost.position, player.transform.position, speed * Time.deltaTime);
-                if(ghost.position.x > player.transform.position.x)
-                {
-                    ghostSprite.flipX = true;
-                }
-                else
-                {
-                    ghostSprite.flipX = false;
-                }
-                ghostAnim.SetBool("Attacking", true);
-            }
-            else
-            {
-                ghostAnim.SetBool("Attacking", false);
-            }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerDetected = true;
+            timer = 0;
+            active = true;
+            PlayMusicalBox();
+        }
+    }
 
-            if (timer<= timeToRestart)
-            {
-                timer = 0;
-                active = true;
-                PlayMusicalBox();
-            }
-        }    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerDetected = false;
+            audioSource.Stop();
+            ghostAnim.Play("Stop");
+            anim.Play("Close");
+        }
     }
 
     void PlayMusicalBox()
